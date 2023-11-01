185c1858a977757be3140aa472e4e378e07ce36c
Aniekwe Onyekachi Godwin <aniekweonyekachi@gmail.com>
2023-11-01 12:51:01 -0700

    C - Binary trees

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates binary tree node
+ * @parent: ptr to parent of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: if error occurs - NULL
+ * 	   otherwise - ptr to the new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
