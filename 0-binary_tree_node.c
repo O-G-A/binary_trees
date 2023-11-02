5dae714b2b66ca60d3a778c231243da69e39a2c8
Aniekwe Onyekachi Godwin <aniekweonyekachi@gmail.com>
2023-11-02 06:38:25 -0700

    update

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates binary tree node
- * @parent: ptr to parent of the node to create
+ * binary_tree_node - creates a binary tree node
+ * @parent: ptr to the parent of the node to create
  * @value: value to put in the new node
  *
- * Return: if error occurs - NULL
- * 	   otherwise - ptr to the new node
+ * Return: if an error occurs - NULL
+ *         otherwise - a pointer to the new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
